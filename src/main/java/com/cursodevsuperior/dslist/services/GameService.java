8cc1750d296e0d88d0b522ac0ca638ed3e9f47d8
Uedney Morais <uedneymorais@gmail.com>
2023-07-15 15:56:02 -0300

    Database seed updated

--- src/main/java/com/cursodevsuperior/dslist/services/GameService.java
+++ src/main/java/com/cursodevsuperior/dslist/services/GameService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.cursodevsuperior.dslist.dto.GameMinDTO;
 import com.cursodevsuperior.dslist.entities.Game;
 import com.cursodevsuperior.dslist.repositories.GameRepository;
 
